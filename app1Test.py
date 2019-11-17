c6b6f31d5f1da85d3633e35dfae0c5c5e740241e
Christopher Parks <cvp5380@psu.edu>
2019-11-17 16:57:55 -0500

    App1Test fixed w/o exception

--- app1Test.py
+++ app1Test.py
@@ -10,14 +10,26 @@
 
 # Last Date Changed:10/12/2019
 
-import app1, app1Client
-
 import unittest
 
-def test_if_payload_is_recieved(self):
-	a1 = app1()
-	a1client = app1Client()
-	payload = {"message": "This is a test"}
-	a1client.c_ssl.send(payload)
-	result = a1.c_ssl.recv(157778)
-	assertTrue(result)
+class TestApp1(unittest.TestCase):
+    def test_sample(self):
+        print("create an INET, STREAMing socket using SSL")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ssl_sock = ssl.wrap_socket(s,
+                server_side=True,
+                ssl_version=ssl.PROTOCOL_TLSv1,
+                certfile="server.crt",
+                keyfile="server.key")
+        print("bind the socket to a public host, and a well-known port 8080")
+        ssl_sock.bind(("localhost", 8080))
+        ssl_sock.listen(5)
+        print("ciphers: " + str(ssl_sock.cipher()))
+        while True:
+                print("accept connections from outside")
+                (c_ssl, address) = ssl_sock.accept()
+                jsonPayload = c_ssl.recv(157778)
+                print("json payload received: ", jsonPayload)
+if __name__ == '__main__':
+    unittest.main()
+

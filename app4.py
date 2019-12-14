c21ae7023b15349c18e99d36803ef5fd77b67f91
Nahara <nkm5334@psu.edu>
2019-12-14 06:17:06 +0000

    added AES encryption

--- app4.py
+++ app4.py
@@ -23,3 +23,37 @@ try:
 	connection.close()
 except Exception as e:
 	print(e)
+
+
+try:
+	print("Reading the file Payload to encrypt")
+	pad = b' '
+	payload = open('jsonPayload5.json', 'rb')
+	data = payload.read()
+	jsonPayload = data.decode('utf-8')
+
+	payload.close()
+	cipher = AES.new('keykeykeykeykeykeykeykeykeykeyyy'.encode('utf-8'), AES.MODE_CBC, 'This is an IV456'.encode('utf-8'))
+	print("Cipher: ", cipher)
+	def encryptPayload(data, cipher):
+		plaintext = data
+		print(plaintext)
+		length = 16 - (len(plaintext)%16)
+
+		print(length)
+
+		plaintext += length*pad
+		print("Plain text: ", plaintext)
+		ciphertext = cipher.encrypt(plaintext)
+		print(type(ciphertext))
+		print("Ciphertext: ", ciphertext)
+		return ciphertext
+	def testEncrypt(benchmark):
+		result = benchmark(encryptPayload, data, cipher)
+		assert type(result) is bytes
+	ciphertext = encryptPayload(data, cipher)
+	with open('encryptedPayloadMelo.aes', 'wb') as outFile: outFile.write(ciphertext)
+
+except:
+	e = sys.exc_info()[0]
+	print("error: %s" % e)

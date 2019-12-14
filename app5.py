6d6c4a69692e14f179a661f16ba75f669369ae94
Nahara <nkm5334@psu.edu>
2019-12-14 19:52:13 +0000

    docs

--- app5.py
+++ app5.py
@@ -1,10 +1,13 @@
+""" App5 """
 import sys, datetime
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from pymongo import ASCENDING
 
 
-
+"""
+Save workflow status pass/fail and respective timestamp to mongodb database collection
+"""
 def workflowLog(msg):
 	"""Workflow Action Status"""
 	client = MongoClient('localhost', 27017)
@@ -21,10 +24,10 @@ def main():
 
 	try:
 		client = MongoClient('localhost', 27017)
-		db = client.logTeam5
-		collection = db.logs
+		db = client.Team5Logs
+		collection = db.logs5
 
-		for post in db.logs.find():
+		for post in db.logs5.find():
 			print(str(post['_id']))
 	except:
 		e = sys.exc_info()[0]

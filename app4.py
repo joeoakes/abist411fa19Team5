1c3583398eddd67f282ec3159ea081d5a4f38254
Nahara <nkm5334@psu.edu>
2019-12-14 15:45:20 +0000

    Modified app4 to send encrypted payload using RabbitMQ

--- app4.py
+++ app4.py
@@ -1,5 +1,5 @@
 import Pyro4
-from app1Phase2 import workflowLog
+from app5 import workflowLog
 
 import pika
 
@@ -10,50 +10,45 @@ greeting_maker = Pyro4.Proxy(uri)
 print(greeting_maker.get_fortune(name))
 workflowLog('Pass')
 
-try:
-	payload = open('jsonPayload5.json', 'rb')
-	data = payload.read()
-	print("Connecting to Localhost Queue")
-	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-	channel = connection.channel()
-	print("Channel Connected")
-	channel.queue_declare(queue = 'ist411')
-	channel.basic_publish(exchange = '', routing_key = 'ist411', body = data)
-	print("[x] Send Payload")
-	connection.close()
-except Exception as e:
-	print(e)
 
+""" Encryot a json payload using a 32-byte key """
+def encryptPayload(data, cipher):
+	plaintext = data
+	print(plaintext)
+	length = 16 - (len(plaintext)%16)
+	print(length)
+	plaintext += length*pad
+	print("Plain text: ", plaintext)
+	ciphertext = cipher.encrypt(plaintext)
+	print(type(ciphertext))
+	print("Ciphertext: ", ciphertext)
+	return ciphertext
 
 try:
 	print("Reading the file Payload to encrypt")
 	pad = b' '
 	payload = open('jsonPayload5.json', 'rb')
 	data = payload.read()
-	jsonPayload = data.decode('utf-8')
-
-	payload.close()
+	""" AES cipher using a 32-byte key """
 	cipher = AES.new('keykeykeykeykeykeykeykeykeykeyyy'.encode('utf-8'), AES.MODE_CBC, 'This is an IV456'.encode('utf-8'))
-	print("Cipher: ", cipher)
-	def encryptPayload(data, cipher):
-		plaintext = data
-		print(plaintext)
-		length = 16 - (len(plaintext)%16)
-
-		print(length)
-
-		plaintext += length*pad
-		print("Plain text: ", plaintext)
-		ciphertext = cipher.encrypt(plaintext)
-		print(type(ciphertext))
-		print("Ciphertext: ", ciphertext)
-		return ciphertext
-	def testEncrypt(benchmark):
-		result = benchmark(encryptPayload, data, cipher)
-		assert type(result) is bytes
 	ciphertext = encryptPayload(data, cipher)
-	with open('encryptedPayloadMelo.aes', 'wb') as outFile: outFile.write(ciphertext)
+	""" Saving  encypted payload to an aes file """
+	with open('encryptedPayload5.aes', 'wb') as outFile: outFile.write(ciphertext)
+	print("Connecting to Localhost Queue")
+	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+	channel = connection.channel()
+	print("Channel Connected")
+	channel.queue_declare(queue = 'ist411')
+	channel.basic_publish(exchange = '', routing_key = 'ist411', body = ciphertext)
+	print("[x] Send Payload")
+	connection.close()
+except Exception as e:
+	print(e)
 
-except:
-	e = sys.exc_info()[0]
-	print("error: %s" % e)
+
+
+"""  Benchmarking test to assure type of encypted payload is in bytes """
+def testEncrypt(benchmark):
+	result = benchmark(encryptPayload, data, cipher)
+	assert type(result) is bytes
+

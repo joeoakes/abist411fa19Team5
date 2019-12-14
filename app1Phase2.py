72aaff0bf7cc18e13079df99456794d6030f5663
Nahara <nkm5334@psu.edu>
2019-12-14 16:48:38 +0000

    Fixed AES Decryption

--- app1Phase2.py
+++ app1Phase2.py
@@ -1,11 +1,14 @@
 
 import socket, ssl, urllib.request, json
+from app5 import workflowLog
 from pymongo import ASCENDING
 import sys, datetime
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import pprint
 import pika
+from Crypto.Cipher import AES
+
 
 url='https://jsonplaceholder.typicode.com'
 param='/posts/1/comments'
@@ -18,12 +21,6 @@ try:
 	collection = db.logs
 	collection.ensure_index([("timestamp", ASCENDING)])
 	payload=response.read()
-	def workflowLog(msg):
-		"""Workflow Action Status"""
-		entry = {}
-		entry['timestamp'] = datetime.datetime.utcnow()
-		entry['status'] = msg
-		collection.insert(entry)
 
 	print("Client connecting on port 8080 using SSL")
 	clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,31 +30,25 @@ try:
 	c_ssl.connect(('localhost', 8080))
 	c_ssl.send(payload)
 	workflowLog("Connection to app1 established")
-
+	def decryptPayload(data, cipher):
+		decrypted = cipher.decrypt(data)
+		return decrypted
 	print("Connection to localhost")
 	connection = pika.BlockingConnection(pika.ConnectionParameters(host = 'localhost'))
 	channel = connection.channel()
 	print("Queue ist411 created")
 	channel.queue_declare(queue = 'ist411')
+#	aesFile = open('encryptedPayloadMelo.aes', 'rb')
+	cipher = AES.new('keykeykeykeykeykeykeykeykeykeyyy'.encode('utf-8'), AES.MODE_CBC, 'This is an IV456'.encode('utf-8'))
 	def callback(ch, method, properties, body):
-		print("[x] Recieved %r" % body)
+		print("[x] Received %r" % body)
+#		decryptPayload(data, cipher)
+		print("Decrypted Payload: ", decryptPayload(body, cipher))
+		workflowLog("Pass Decryption")
 	channel.basic_consume(queue = 'ist411', on_message_callback = callback, auto_ack = True)
 	print(' [*] Waiting for messages. To exit press CTRL + C')
 	channel.start_consuming()
 
-	def decryptPayload(data, cipher):
-		decrypted = cipher.decrypt(data)
-		return decrypted
-
-	if option == "1":
-		try:
-			print("Decrypted Payload: ", decryptPayload(data, cipher))
-			decryptPayload(data, cipher)
-			workflowLog("Pass Decryption")
-		except Exception as e:
-			print(e)
-			workflowLog('Fail')
-	print("Reading Payload")
 except Exception as e:
 	print(e)
 	print(c_ssl.cipher())

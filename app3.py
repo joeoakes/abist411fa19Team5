df819cb00e616ad313796e9795bf4d05d3a80bb5
Nahara <nkm5334@psu.edu>
2019-12-14 15:52:13 +0000

    Added comments

--- app3.py
+++ app3.py
@@ -1,13 +1,16 @@
 import sys, pysftp, paramiko, app2Phase2, hashlib, hmac, base64, app1Phase2, zlib, gzip
 from email.mime.text import MIMEText
-from app1Phase2  import  workflowLog
+import app5
 import smtplib
 import Pyro4
 
+
+""" Connection option for using SFTP """
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None
 cinfo = {'cnopts':cnopts, 'host':'oz-ist-linux-oakes', 'username':'ftpuser', 'password':'test1234', 'port':100}
 
+""" Set up for email  message """
 fromAddress = 'nkm5334@psu.edu'
 subject = 'JSON Payload'
 toAddress =['nkm5334@psu.edu', 'asw5310@psu.edu', 'tpl5148@psu.edu', 'cvp5380@psu.edu', 'bmy5076@psu.edu']
@@ -22,6 +25,7 @@ class GreetingMaker(object):
 try:
 	with pysftp.Connection(**cinfo) as sftp:
 		print("Connection made")
+		""" SFTP methods to transfer and remove files """
 		print("getting jsonPayload5.json file")
 		sftp.get('/home/ftpuser/jsonPayload5.json')
 		sftp.remove('/home/ftpuser/jsonPayload5.json')
@@ -29,6 +33,10 @@ try:
 		payload = open('jsonPayload5.json', 'rb')
 		data = payload.read()
 		payload.close()
+		"""
+			Establishing a key to generate digests
+			Same key is used in app2 to ascertain data integrity
+		"""
 		key = "5411"
 		key = bytes(key, 'UTF-8')
 		signature  = hmac.new(key, data, hashlib.sha256).hexdigest()
@@ -42,17 +50,20 @@ try:
 		signature5 = base64.encodestring(signature4)
 		print(signature5)
 #		app2Phase2.verifyHash(app2Phase2.signature2, signature_)
+		""" Compare two keys """
 		compare = hmac.compare_digest(signature5, signature_)
 		print("Hash Verified:", compare)
-		app1Phase2.workflowLog("Hash Verified")
+		workflowLog("Hash Verified")
 		payloadN = data.decode('utf-8')
-		app1Phase2.workflowLog("Pass")
+		workflowLog("Pass")
+		""" Compress a payload using gzip """
 		def compressPayload(data):
 			payloadComp = gzip.compress(data)
 			return payloadComp
 		compressPayload(data)
 		app1Phase2.workflowLog("payload compressed")
 		print("JSON Payload Compressed")
+		""" Send an email message containg a payload """
 		def sendEmail(payload,subject, fromAddress, toAddress):
 			email_msg=payload
 			msg = MIMEText(email_msg)
@@ -63,11 +74,11 @@ try:
 			s = smtplib.SMTP_SSL('authsmtp.psu.edu', 465)
 			s.sendmail(fromAddress, toAddress, msg.as_string())
 		sendEmail(payloadN, subject, fromAddress, toAddress)
-		app1Phase2.workflowLog("email sent")
+		app5.workflowLog("email sent")
 		daemon = Pyro4.Daemon()
 		uri = daemon.register(GreetingMaker)
 		print("Ready. Object uri = ", uri)
 		daemon.requestLoop()
 except:
 	print("Log exception 2:", sys.exc_info()[0])
-	app1Phase2.workflowLog("Fail")
+	app5.workflowLog("Fail")

4b23c0be9a824e97077522df98276601969b94e3
Brian <bmy5076@psu.edu>
2019-12-14 05:25:24 +0000

    Attempted adding benchmarking code for app1

--- app1Phase2.py
+++ app1Phase2.py
@@ -1,4 +1,5 @@
-
+import requests
+import pytest
 import socket, ssl, urllib.request, json
 from pymongo import ASCENDING
 import sys, datetime
@@ -10,35 +11,40 @@ import pprint
 url='https://jsonplaceholder.typicode.com'
 param='/posts/1/comments'
 
-try:
-	print('Url:', url+param)
-	response=urllib.request.urlopen(url+param)
-	client = MongoClient('localhost', 27017)
-	db = client.logTeam5
-	collection = db.logs
-	collection.ensure_index([("timestamp", ASCENDING)])
-	payload=response.read()
-	def workflowLog(msg):
-		"""Workflow Action Status"""
-		entry = {}
-		entry['timestamp'] = datetime.datetime.utcnow()
-		entry['status'] = msg
-		collection.insert(entry)
+def f(x):
+	try:
+		print('Url:', url+param)
+		response=urllib.request.urlopen(url+param)
+		client = MongoClient('localhost', 27017)
+		db = client.logTeam5
+		collection = db.logs
+		collection.ensure_index([("timestamp", ASCENDING)])
+		payload=response.read()
+		def workflowLog(msg):
+			"""Workflow Action Status"""
+			entry = {}
+			entry['timestamp'] = datetime.datetime.utcnow()
+			entry['status'] = msg
+			collection.insert(entry)
 
-	print("Client connecting on port 8080 using SSL")
-	clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	c_ssl = ssl.wrap_socket(clientsocket,
-		ca_certs="server.crt",
-		cert_reqs=ssl.CERT_REQUIRED)
-	c_ssl.connect(('localhost', 8080))
-	c_ssl.send(payload)
-	workflowLog("Connection to app1 established")
-except Exception as e:
-	print(e)
-	print(c_ssl.cipher())
-	c_ssl.close()
-	workflowLog('Fail')
+		print("Client connecting on port 8080 using SSL")
+		clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		c_ssl = ssl.wrap_socket(clientsocket,
+			ca_certs="server.crt",
+			cert_reqs=ssl.CERT_REQUIRED)
+		c_ssl.connect(('localhost', 8080))
+		c_ssl.send(payload)
+		workflowLog("Connection to app1 established")
+		return x == True
+	except Exception as e:
+		print(e)
+		print(c_ssl.cipher())
+		c_ssl.close()
+		workflowLog('Fail')
+		return x == False
 
+def test_benchmark(benchmark):
+	assert f(x) == True
 
 with open('curlApp3.json', 'w') as outFile:
 	jsonObj = outFile.write(json.dumps(payload.decode('utf8')))
---
+++ test_app1Phase2.py
@@ -0,0 +1,8 @@
+import app1Phase2
+
+# content of test_class.py
+class TestClass:
+	def test_app(benchmark):
+		result = benchmark(x)
+
+		assert result == True

060cfa0bc1a243c2333b84b2e44b2f1b425bd4f6
Nahara <nkm5334@psu.edu>
2019-12-14 16:19:02 +0000

    Fixed method

--- app5.py
+++ app5.py
@@ -3,18 +3,35 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from pymongo import ASCENDING
 
-try:
+
+
+def workflowLog(msg):
+	"""Workflow Action Status"""
 	client = MongoClient('localhost', 27017)
-	db = client.logTeam5
-	collection = db.logs
-#	collection.insert(collection)
+	db = client.Team5Logs
+	collection = db.logs5
+	collection.ensure_index([("timestamp", ASCENDING)])
+	entry = {}
+	entry['timestamp'] = datetime.datetime.utcnow()
+	entry['status'] = msg
+	collection.insert(entry)
 
-	for post in db.logs.find():
-		print(str(post['_id']))
-except:
-	e = sys.exc_info()[0]
-	print("error: %s" % e)
 
+def main():
+
+	try:
+		client = MongoClient('localhost', 27017)
+		db = client.logTeam5
+		collection = db.logs
+
+		for post in db.logs.find():
+			print(str(post['_id']))
+	except:
+		e = sys.exc_info()[0]
+		print("error: %s" % e)
+
+if __name__ == "__main__":
+	main()
 
 
 
